1559c84062fed6cee6ef2302fd2f72f4fb9e4e92
eli <elizabeth.johana.gonzalez@gmail.com>
2021-03-29 14:38:17 -0300

    update

--- models_profiles.py
+++ models_profiles.py
@@ -16,6 +16,9 @@ G    = G.value;   # Gravitational constant (m3.kg-1.s-2)
 pc   = pc.value # 1 pc (m)
 Msun = M_sun.value # Solar mass (kg)
 
+def g(x,disp): 
+    return (1./(np.sqrt(2*np.pi)*disp))*np.exp(-0.5*(x/disp)**2)
+
 def Gamma(Roff,s_off):
     return (Roff/s_off**2)*np.exp(-1.*(Roff/s_off))
 
@@ -520,3 +523,286 @@ def Sigma_NFW_miss_parallel(r,z,M200,s_off = None, tau = 0.2,
         S_miss = np.append(S_miss,s)
             
     return S_miss
+
+##############################################
+
+def Rayleigh_elip(roff,theta,s_off,q):
+    Roff = roff*np.sqrt(q*(np.cos(theta))**2 + (np.sin(theta))**2 / q)
+    return (Roff/s_off**2)*np.exp(-0.5*(Roff/s_off)**2)
+
+
+def Sigma_NFW_miss_elip(R,z,M200,s_off = None, tau = 0.2,
+                   c200 = None, P_Roff = Rayleigh_elip, 
+                   qmiss = 0.8, cosmo=cosmo):
+                       
+    
+    '''
+    Misscentred density NFW profile
+    F_Eq12
+    '''
+    
+
+    
+    R200 = R200_NFW(M200,z,cosmo)
+    
+    if not c200:
+        c200 = c200_duffy(M200*cosmo.h,z)
+
+    if not s_off:
+        s_off = tau*R200
+
+
+    def Smiss(Rs,theta,R):
+        # F_Eq13
+        Sm = Sigma_NFW((np.sqrt(R**2+Rs**2-2.*Rs*R*np.cos(theta))),z,M200,c200,cosmo)
+        return Sm
+
+    
+    
+    integral = []
+    for r in R:
+        argumento = lambda t,x: Smiss(x,t,r)*P_Roff(x,t,s_off,qmiss)
+        integral  += [(1./2.*np.pi)*(integrate.dblquad(argumento, 0, np.inf, lambda x: 0, lambda x: 2.*np.pi, epsabs=1.e-02, epsrel=1.e-02)[0])]
+        
+    return integral
+
+
+def Delta_Sigma_NFW_miss_elip(R,z,M200,s_off = None, tau = 0.2,
+                         c200 = None, P_Roff = Rayleigh_elip, 
+                         qmiss = 0.8, cosmo=cosmo):	
+    
+    '''
+    Misscentred density contraste for NFW
+    
+    '''
+  
+    R200 = R200_NFW(M200,z,cosmo)
+    
+        
+    if not c200:
+        c200 = c200_duffy(M200*cosmo.h,z)
+
+    if not s_off:
+        s_off = tau*R200
+
+
+    integral = []
+    for r in R:
+        argumento = lambda x: Sigma_NFW_miss_elip([x],z,M200,s_off,tau,c200,P_Roff,qmiss,cosmo)[0]*x
+        integral  += [integrate.quad(argumento, 0, r, epsabs=1.e-02, epsrel=1.e-02)[0]]
+
+    DS_off    = (2./R**2)*integral - Sigma_NFW_miss_elip(R,z,M200,s_off,tau,c200,P_Roff,qmiss,cosmo)
+
+    return DS_off
+
+    
+
+
+def GAMMA_components_miss_elip(R,z,M200,ellip,s_off = None, tau = 0.2,
+                         c200 = None, P_Roff = Rayleigh_elip, 
+                         qmiss = 0.8, cosmo=cosmo, return_S2 = False):	
+    
+    '''
+    Misscentred quadrupole components for NFW
+    
+    '''
+  
+    R200 = R200_NFW(M200,z,cosmo)
+    
+        
+    if not c200:
+        c200 = c200_duffy(M200*cosmo.h,z)
+
+    if not s_off:
+        s_off = tau*R200
+
+    def monopole(R):
+        return Sigma_NFW(R,z,M200,c200,cosmo=cosmo)
+    
+    def monopole_off(R):
+        return Sigma_NFW_miss_elip([R],z,M200,s_off,tau,c200,P_Roff,qmiss,cosmo)[0]
+    
+    def quadrupole(R):
+        m0p = derivative(monopole,R,dx=1e-5)
+        return m0p*R
+        
+    def S2miss(Rs,theta,R):
+        # F_Eq13
+        S2m = quadrupole(np.sqrt(R**2+Rs**2-2.*Rs*R*np.cos(theta)))
+        return S2m
+
+    def psi2_off(R):
+        def arg(x):
+            return (x**3)*monopole_off(x)
+        argumento = lambda x: arg(x)
+        integral = integrate.quad(argumento, 0, R, epsabs=1.e-01, epsrel=1.e-01)[0]
+        return integral*(-2./(R**2))        
+
+    S2off = []
+    p2off = []
+    S0off = Sigma_NFW_miss_elip(R,z,M200,s_off,tau,c200,P_Roff,qmiss,cosmo)
+
+    for r in R:
+        argumento = lambda t,x: S2miss(x,t,r)*P_Roff(x,t,s_off,qmiss)
+        S2off  += [(1./2.*np.pi)*(integrate.dblquad(argumento, 0, np.inf, lambda x: 0, lambda x: 2.*np.pi, epsabs=1.e-02, epsrel=1.e-02)[0])]
+        p2off  += [psi2_off(r)]
+
+    
+    '''
+    vU_Eq10
+    
+    '''
+    
+    p2off = np.array(p2off)
+    S2off = np.array(S2off)
+    S0off = np.array(S0off)
+    
+    gt2off = ellip*((-6.*p2off/R**2) - 2.*S0off + S2off)
+    gx2off = ellip*((-6.*p2off/R**2) - 4.*S0off)
+    
+    if return_S2:
+        return [gt2off,gx2off,S2off]
+    else:
+        return [gt2off,gx2off]
+
+def GAMMA_components_miss_elip_unpack(minput):
+	return GAMMA_components_miss_elip(*minput)
+
+def GAMMA_components_miss_elip_parallel(r,z,M200,ellip,
+                                   s_off = None, tau = 0.2,
+                                   c200 = None, P_Roff = Rayleigh_elip, 
+                                   qmiss = 0.8, cosmo=cosmo,
+                                   return_S2 = False,
+                                   ncores=4):	
+	
+    if ncores > len(r):
+        ncores = len(r)
+    
+    
+    slicer = int(round(len(r)/float(ncores), 0))
+    slices = ((np.arange(ncores-1)+1)*slicer).astype(int)
+    slices = slices[(slices <= len(r))]
+    r_splitted = np.split(r,slices)
+    
+    ncores = len(r_splitted)
+    
+    z      = [z]*ncores
+    M200   = [M200]*ncores
+    ellip  = [ellip]*ncores
+    s_off  = [s_off]*ncores
+    tau    = [tau]*ncores
+    c200   = [c200]*ncores
+    P_Roff = [P_Roff]*ncores
+    cosmo  = [cosmo]*ncores
+    rS2    = [return_S2]*ncores
+    qmiss    = [qmiss]*ncores
+        
+    entrada = np.array([r_splitted,z,M200,ellip,s_off,tau,c200,P_Roff,qmiss,cosmo,rS2]).T
+    
+    pool = Pool(processes=(ncores))
+    salida=np.array(pool.map(GAMMA_components_miss_elip_unpack, entrada))
+    pool.terminate()
+
+    GT_miss = np.array([])
+    GX_miss = np.array([])
+    S2_miss = np.array([])
+    
+    for s in salida:
+        if return_S2:
+            gt,gx,s2 = s
+            GT_miss = np.append(GT_miss,gt)
+            GX_miss = np.append(GX_miss,gx)
+            S2_miss = np.append(S2_miss,s2)
+        else:
+            gt,gx = s
+            GT_miss = np.append(GT_miss,gt)
+            GX_miss = np.append(GX_miss,gx)
+
+    if return_S2:
+        return [GT_miss,GX_miss,S2_miss]
+    else:
+        return [GT_miss,GX_miss]
+    
+    
+def Delta_Sigma_NFW_miss_elip_unpack(minput):
+	return Delta_Sigma_NFW_miss_elip(*minput)
+
+def Delta_Sigma_NFW_miss_elip_parallel(r,z,M200,s_off = None, tau = 0.2,
+                         c200 = None, P_Roff = Rayleigh_elip, 
+                         qmiss = 0.8,cosmo=cosmo,ncores=4):	
+	
+    if ncores > len(r):
+        ncores = len(r)
+    
+    
+    slicer = int(round(len(r)/float(ncores), 0))
+    slices = ((np.arange(ncores-1)+1)*slicer).astype(int)
+    slices = slices[(slices <= len(r))]
+    r_splitted = np.split(r,slices)
+    
+    ncores = len(r_splitted)
+    
+    z      = [z]*ncores
+    M200   = [M200]*ncores
+    s_off  = [s_off]*ncores
+    tau    = [tau]*ncores
+    c200   = [c200]*ncores
+    P_Roff = [P_Roff]*ncores
+    cosmo  = [cosmo]*ncores
+    qmiss  = [qmiss]*ncores
+        
+    entrada = np.array([r_splitted,z,M200,s_off,tau,c200,P_Roff,qmiss,cosmo]).T
+    
+    pool = Pool(processes=(ncores))
+    salida=np.array(pool.map(Delta_Sigma_NFW_miss_elip_unpack, entrada))
+    pool.terminate()
+
+    DS_miss = np.array([])
+    
+    for s in salida:
+        DS_miss = np.append(DS_miss,s)
+            
+    return DS_miss
+
+
+def Sigma_NFW_miss_unpack(minput):
+	return Sigma_NFW_miss_elip(*minput)
+
+
+def Sigma_NFW_miss_elip_parallel(r,z,M200,s_off = None, tau = 0.2,
+                         c200 = None, P_Roff = Rayleigh_elip, 
+                         qmiss = 0.8,cosmo=cosmo,ncores=4):	
+	
+    if ncores > len(r):
+        ncores = len(r)
+    
+    
+    slicer = int(round(len(r)/float(ncores), 0))
+    slices = ((np.arange(ncores-1)+1)*slicer).astype(int)
+    slices = slices[(slices <= len(r))]
+    r_splitted = np.split(r,slices)
+    
+    ncores = len(r_splitted)
+    
+    z      = [z]*ncores
+    M200   = [M200]*ncores
+    s_off  = [s_off]*ncores
+    tau    = [tau]*ncores
+    c200   = [c200]*ncores
+    P_Roff = [P_Roff]*ncores
+    cosmo  = [cosmo]*ncores
+    qmiss  = [qmiss]*ncores
+        
+    entrada = np.array([r_splitted,z,M200,s_off,tau,c200,P_Roff,qmiss,cosmo]).T
+    
+    pool = Pool(processes=(ncores))
+    salida=np.array(pool.map(Sigma_NFW_miss_elip_unpack, entrada))
+    pool.terminate()
+
+    S_miss = np.array([])
+    
+    for s in salida:
+        S_miss = np.append(S_miss,s)
+            
+    return S_miss
+

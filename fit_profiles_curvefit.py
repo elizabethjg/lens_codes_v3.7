6a2d4f9553d81ac7169618a0b0817cce35e6661f
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2021-09-02 13:53:20 -0300

    change res computing

--- fit_profiles_curvefit.py
+++ fit_profiles_curvefit.py
@@ -137,8 +137,7 @@ class Sigma_fit:
                 chired  = chi_red(ajuste,Sigma,err,2)	
                 
                 # compute residuals Eq6-Meneghetti et al 2014
-                lerror  = err/(Sigma*np.log(10.))
-                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(Sigma),lerror,1))
+                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(Sigma),np.ones(len(Sigma),1))
 
                 
                 yplot   = NFW_profile(xplot,R200,c200)
@@ -182,8 +181,7 @@ class Sigma_fit:
                 e_c200 = 0.
                 
                 # compute residuals Eq6-Meneghetti et al 2014
-                lerror  = err/(Sigma*np.log(10.))
-                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(Sigma),lerror,0))
+                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(Sigma),np.ones(len(Sigma),0))
 
             
             except:
@@ -245,8 +243,7 @@ class rho_fit:
                 chired  = chi_red(ajuste,rho,err,2)	
                 
                 # compute residuals Eq6-Meneghetti et al 2014
-                lerror  = err/(rho*np.log(10.))
-                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(rho),lerror,1))
+                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(rho),np.ones(len(rho),1))
                 
                 yplot   = NFW_profile(xplot,R200,c200)
             
@@ -280,8 +277,7 @@ class rho_fit:
                 
                 chired  = chi_red(ajuste,rho,err,1)	
                 # compute residuals Eq6-Meneghetti et al 2014
-                lerror  = err/(Sigma*np.log(10.))
-                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(rho),lerror,0))
+                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(rho),np.ones(len(rho),0))
                 yplot   = NFW_profile(xplot,R200)
                 
                 #calculo de c usando la relacion de Duffy et al 2008

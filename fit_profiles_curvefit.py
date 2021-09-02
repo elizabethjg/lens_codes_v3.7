ecb411e0d2aa531c4d434c10d06144b200b2eaf6
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2021-09-02 13:56:05 -0300

    debug

--- fit_profiles_curvefit.py
+++ fit_profiles_curvefit.py
@@ -137,7 +137,7 @@ class Sigma_fit:
                 chired  = chi_red(ajuste,Sigma,err,2)	
                 
                 # compute residuals Eq6-Meneghetti et al 2014
-                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(Sigma),np.ones(len(Sigma),1))
+                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(Sigma),np.ones(len(Sigma)),1))
 
                 
                 yplot   = NFW_profile(xplot,R200,c200)
@@ -181,7 +181,7 @@ class Sigma_fit:
                 e_c200 = 0.
                 
                 # compute residuals Eq6-Meneghetti et al 2014
-                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(Sigma),np.ones(len(Sigma),0))
+                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(Sigma),np.ones(len(Sigma)),0))
 
             
             except:
@@ -243,7 +243,7 @@ class rho_fit:
                 chired  = chi_red(ajuste,rho,err,2)	
                 
                 # compute residuals Eq6-Meneghetti et al 2014
-                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(rho),np.ones(len(rho),1))
+                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(rho),np.ones(len(rho)),1))
                 
                 yplot   = NFW_profile(xplot,R200,c200)
             
@@ -277,7 +277,7 @@ class rho_fit:
                 
                 chired  = chi_red(ajuste,rho,err,1)	
                 # compute residuals Eq6-Meneghetti et al 2014
-                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(rho),np.ones(len(rho),0))
+                res     = np.sqrt(chi_red(np.log10(ajuste),np.log10(rho),np.ones(len(rho)),0))
                 yplot   = NFW_profile(xplot,R200)
                 
                 #calculo de c usando la relacion de Duffy et al 2008

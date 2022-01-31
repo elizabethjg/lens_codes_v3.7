6e9ef6bde34ee6b82ff0e8d3d8139d01dac57a12
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2022-01-31 05:32:58 -0300

    updated

--- models_profiles.py
+++ models_profiles.py
@@ -15,6 +15,7 @@ cvel = c.value;   # Speed of light (m.s-1)
 G    = G.value;   # Gravitational constant (m3.kg-1.s-2)
 pc   = pc.value # 1 pc (m)
 Msun = M_sun.value # Solar mass (kg)
+params = {'flat': True, 'H0': 70.0, 'Om0': 0.25, 'Ob0': 0.044, 'sigma8': 0.8, 'ns': 0.95}
 
 def g(x,disp): 
     return (1./(np.sqrt(2*np.pi)*disp))*np.exp(-0.5*(x/disp)**2)
@@ -248,38 +249,7 @@ def quadrupole(R,z,M200,c200 = None,cosmo=cosmo):
     return q
 
 
-def GAMMA_components(R,z,ellip,M200,c200 = None,cosmo=cosmo):
-   
-    '''
-    Quadrupole term defined as (d(Sigma)/dr)*r
-    
-    '''
-    
-    def monopole(R):
-        return Sigma_NFW(R,z,M200,c200,cosmo=cosmo)/1.e6**2
-    
-    m0p = derivative(monopole,R,dx=1e-5)
-    q   =  m0p*R
-    
-    '''
-    vU_Eq10
-    
-    '''
-    
-    integral = []
-    for r in R:
-        argumento = lambda x: (x**3)*monopole(x)
-        integral += [integrate.quad(argumento, 0, r, epsabs=1.e-01, epsrel=1.e-01)[0]]
-        
-    p2 = integral*(-2./(R**2))
-    m  = monopole(R)
-    
-    gt2 = ellip*((-6*p2/R**2) - 2.*m + q)
-    gx2 = ellip*((-6*p2/R**2) - 4.*m)
-    
-    return [gt2,gx2]
-
-def Sigma_NFW_2h(R,z,M200,c200,cosmo_params):
+def Sigma_NFW_2h(R,z,M200,c200,cosmo_params=params,terms='1h'):
     
     from colossus.lss import bias
     from colossus.halo import profile_nfw
@@ -293,13 +263,51 @@ def Sigma_NFW_2h(R,z,M200,c200,cosmo_params):
     outer_term = profile_outer.OuterTermCorrelationFunction(z = z, bias = b)
     pNFW = profile_nfw.NFWProfile(M = M200, mdef = '200c', z = z, c = c200, outer_terms = [outer_term])
     
-    # Outer term integrated up to 50Mpc (Luo et al. 2017, Niemic et al 2017)
-    s_in2  = pNFW.surfaceDensityInner(R*1.e3)
-    s_out2 = pNFW.surfaceDensityOuter(R*1.e3, interpolate=False, accuracy=0.01, max_r_integrate=100e3)
+    # Outer term integrated up to 100Mpc (Luo et al. 2017, Niemic et al 2017)
     
-    return (s_in + s_out)/(1.e3**2)
+    if terms == '1h':
+        s_in  = pNFW.surfaceDensityInner(R*1.e3)
+        s = s_in
+    elif terms == '2h':
+        s_out = pNFW.surfaceDensityOuter(R*1.e3, interpolate=False, accuracy=0.01, max_r_integrate=100e3)
+        s = s_out
+    elif terms == '1h+2h':
+        s_in  = pNFW.surfaceDensityInner(R*1.e3)
+        s_out = pNFW.surfaceDensityOuter(R*1.e3, interpolate=False, accuracy=0.01, max_r_integrate=100e3)
+        s = s_in + s_out
+    
+    return s/(1.e3**2)
 
-def Delta_Sigma_NFW_2h(R,z,M200,c200,cosmo_params):
+def Sigma_Ein_2h(R,z,M200,c200,alpha,cosmo_params=params,terms='1h'):
+    
+    from colossus.lss import bias
+    from colossus.halo import profile_einasto
+    from colossus.halo import profile_outer
+    from colossus.cosmology import cosmology  
+    cosmology.addCosmology('MyCosmo', cosmo_params)
+    cosmo = cosmology.setCosmology('MyCosmo')
+
+    b = bias.haloBias(M200, model = 'tinker10', z = z, mdef = '200c')
+    
+    outer_term = profile_outer.OuterTermCorrelationFunction(z = z, bias = b)
+    p = profile_einasto.EinastoProfile(M = M200, mdef = '200c', z = z, c = c200, alpha = alpha, outer_terms = [outer_term])
+    
+    # Outer term integrated up to 100Mpc (Luo et al. 2017, Niemic et al 2017)
+    
+    if terms == '1h':
+        s_in  = p.surfaceDensityInner(R*1.e3)
+        s = s_in
+    elif terms == '2h':
+        s_out = p.surfaceDensityOuter(R*1.e3, interpolate=False, accuracy=0.01, max_r_integrate=100e3)
+        s = s_out
+    elif terms == '1h+2h':
+        s_in  = p.surfaceDensityInner(R*1.e3)
+        s_out = p.surfaceDensityOuter(R*1.e3, interpolate=False, accuracy=0.01, max_r_integrate=100e3)
+        s = s_in + s_out
+    
+    return s/(1.e3**2)
+
+def Delta_Sigma_NFW_2h(R,z,M200,c200,cosmo_params=params,terms='1h'):
     from colossus.lss import bias
     from colossus.halo import profile_nfw
     from colossus.halo import profile_outer
@@ -313,15 +321,22 @@ def Delta_Sigma_NFW_2h(R,z,M200,c200,cosmo_params):
     pNFW = profile_nfw.NFWProfile(M = M200, mdef = '200c', z = z, c = c200, outer_terms = [outer_term])    
     
     # Outer term integrated up to 50Mpc (Luo et al. 2017, Niemic et al 2017)
-    ds_in  = pNFW.deltaSigmaInner(R*1.e3)
-    ds_out = pNFW.deltaSigmaOuter(R*1.e3, interpolate=False, interpolate_surface_density=False, accuracy=0.01, max_r_integrate=100e3)
+    if terms == '1h':
+        ds_in  = pNFW.deltaSigmaInner(R*1.e3)
+        ds = ds_in
+    elif terms == '2h':
+        ds_out = pNFW.deltaSigmaOuter(R*1.e3, interpolate=False, interpolate_surface_density=False, accuracy=0.01, max_r_integrate=100e3)
+        ds = ds_out
+    elif terms == '1h+2h':
+        ds_in  = pNFW.deltaSigmaInner(R*1.e3)
+        ds_out = pNFW.deltaSigmaOuter(R*1.e3, interpolate=False, interpolate_surface_density=False, accuracy=0.01, max_r_integrate=100e3)
+        ds = ds_in + ds_out
     
-    return (ds_in + ds_out)/(1.e3**2)
+    return ds/(1.e3**2)
 
-def Sigma_NFW_only2h(R,z,M200,c200,cosmo_params):
-    
+def Delta_Sigma_Ein_2h(R,z,M200,c200,alpha,cosmo_params=params,terms='1h'):
     from colossus.lss import bias
-    from colossus.halo import profile_nfw
+    from colossus.halo import profile_einasto
     from colossus.halo import profile_outer
     from colossus.cosmology import cosmology  
     cosmology.addCosmology('MyCosmo', cosmo_params)
@@ -330,34 +345,24 @@ def Sigma_NFW_only2h(R,z,M200,c200,cosmo_params):
     b = bias.haloBias(M200, model = 'tinker10', z = z, mdef = '200c')
     
     outer_term = profile_outer.OuterTermCorrelationFunction(z = z, bias = b)
-    pNFW = profile_nfw.NFWProfile(M = M200, mdef = '200c', z = z, c = c200, outer_terms = [outer_term])    
+    p = profile_einasto.EinastoProfile(M = M200, mdef = '200c', z = z, c = c200, alpha = alpha, outer_terms = [outer_term])    
     
     # Outer term integrated up to 50Mpc (Luo et al. 2017, Niemic et al 2017)
-    s_in  = pNFW.surfaceDensityInner(R*1.e3)
-    s_out = pNFW.surfaceDensityOuter(R*1.e3, interpolate=False, accuracy=0.01, max_r_integrate=100e3)
+    if terms == '1h':
+        ds_in  = p.deltaSigmaInner(R*1.e3)
+        ds = ds_in
+    elif terms == '2h':
+        ds_out = p.deltaSigmaOuter(R*1.e3, interpolate=False, interpolate_surface_density=False, accuracy=0.01, max_r_integrate=100e3)
+        ds = ds_out
+    elif terms == '1h+2h':
+        ds_in  = p.deltaSigmaInner(R*1.e3)
+        ds_out = p.deltaSigmaOuter(R*1.e3, interpolate=False, interpolate_surface_density=False, accuracy=0.01, max_r_integrate=100e3)
+        ds = ds_in + ds_out
     
-    return (s_out)/(1.e3**2)
+    return ds/(1.e3**2)
 
-def Delta_Sigma_NFW_only2h(R,z,M200,c200,cosmo_params):
-    from colossus.lss import bias
-    from colossus.halo import profile_nfw
-    from colossus.halo import profile_outer
-    from colossus.cosmology import cosmology  
-    cosmology.addCosmology('MyCosmo', cosmo_params)
-    cosmo = cosmology.setCosmology('MyCosmo')
 
-    b = bias.haloBias(M200, model = 'tinker10', z = z, mdef = '200c')
-    
-    outer_term = profile_outer.OuterTermCorrelationFunction(z = z, bias = b)
-    pNFW = profile_nfw.NFWProfile(M = M200, mdef = '200c', z = z, c = c200, outer_terms = [outer_term])    
-    
-    # Outer term integrated up to 50Mpc (Luo et al. 2017, Niemic et al 2017)
-    ds_in  = pNFW.deltaSigmaInner(R*1.e3)
-    ds_out = pNFW.deltaSigmaOuter(R*1.e3, interpolate=False, interpolate_surface_density=False, accuracy=0.01, max_r_integrate=100e3)
-    
-    return (ds_out)/(1.e3**2)
-
-def GAMMA_components_2h(R,z,ellip,M200,c200,cosmo_params):
+def GAMMA_components(R,z,ellip,M200,c200 = None,terms='1h',cosmo_params=params,pname='NFW',alpha=0.3):
    
     '''
     Quadrupole term defined as (d(Sigma)/dr)*r
@@ -365,9 +370,12 @@ def GAMMA_components_2h(R,z,ellip,M200,c200,cosmo_params):
     '''
     
     def monopole(R):
-        return Sigma_NFW_2h(R,z,M200,c200,cosmo_params)
+        if pname == 'NFW':
+            return Sigma_NFW_2h(R,z,M200,c200,terms=terms,cosmo_params=cosmo_params)
+        elif pname == 'Einasto':
+            return Sigma_Ein_2h(R,z,M200,c200,alpha,terms=terms,cosmo_params=cosmo_params)
     
-    m0p = derivative(monopole,R,dx=1e-5)
+    m0p = derivative(monopole,R,dx=1e-4)
     q   =  m0p*R
     
     '''
@@ -383,42 +391,12 @@ def GAMMA_components_2h(R,z,ellip,M200,c200,cosmo_params):
     p2 = integral*(-2./(R**2))
     m  = monopole(R)
     
-    gt2 = ellip*((-6*p2/R**2) - 2.*m + q)
-    gx2 = ellip*((-6*p2/R**2) - 4.*m)
+    gt = ellip*((-6*p2/R**2) - 2.*m + q)
+    gx = ellip*((-6*p2/R**2) - 4.*m)
     
-    return [gt2,gx2]
+    return [gt,gx]
 
-def GAMMA_components_only2h(R,z,ellip,M200,c200,cosmo_params):
-   
-    '''
-    Quadrupole term defined as (d(Sigma)/dr)*r
-    
-    '''
-    
-    def monopole(R):
-        return Sigma_NFW_only2h(R,z,M200,c200,cosmo_params)
-    
-    m0p = derivative(monopole,R,dx=1e-5)
-    q   =  m0p*R
-    
-    '''
-    vU_Eq10
-    
-    '''
-    
-    integral = []
-    for r in R:
-        argumento = lambda x: (x**3)*monopole(x)
-        integral += [integrate.quad(argumento, 0, r, epsabs=1.e-01, epsrel=1.e-01)[0]]
-        
-    p2 = integral*(-2./(R**2))
-    m  = monopole(R)
-    
-    gt2 = ellip*((-6*p2/R**2) - 2.*m + q)
-    gx2 = ellip*((-6*p2/R**2) - 4.*m)
-    
-    return [gt2,gx2]
-
+### MISCENTRED
 
 def Sigma_NFW_miss(R,z,M200,s_off = None, tau = 0.2,
                    c200 = None, P_Roff = Gamma, cosmo=cosmo):
